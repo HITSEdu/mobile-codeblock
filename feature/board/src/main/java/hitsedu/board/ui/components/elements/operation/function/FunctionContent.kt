93d14dae2e31a5a8d2164a02e7a6927fe0041bfa
YungOleg <bilk5899@gmail.com>
2025-05-17 00:43:21 +0700

    chore(ui): adding to the previous commit

--- feature/board/src/main/java/hitsedu/board/ui/components/elements/operation/function/FunctionContent.kt
+++ feature/board/src/main/java/hitsedu/board/ui/components/elements/operation/function/FunctionContent.kt
@@ -4,5 +4,5 @@ import androidx.compose.runtime.Composable
 
 @Composable
 fun FunctionContent() {
-
+    //TODO("create function content")
 }
---
+++ feature/board/src/main/java/hitsedu/board/ui/components/elements/operation/output/actions/Output.kt
@@ -0,0 +1,15 @@
+package hitsedu.board.ui.components.elements.operation.output.actions
+
+import androidx.compose.runtime.Composable
+import hitsedu.board.ui.BoardViewModel
+import hitsedu.ui_kit.models.ScopeUIO
+import hitsedu.ui_kit.models.operation.OperationOutputUIO
+
+@Composable
+fun Output(
+    parentScope: ScopeUIO,
+    output: OperationOutputUIO,
+    viewModel: BoardViewModel,
+) {
+    // TODO("create output ui")
+}
